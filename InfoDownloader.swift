cad7519178324b4aa16008308677b499e8481aa7
Cori Mims <cdmims21@gmail.com>
2017-05-26 17:14:48 -0400

    Added files to project.

---
+++ InfoDownloader.swift
@@ -0,0 +1,9 @@
+//
+//  InfoDownloader.swift
+//  StarWarsAPI
+//
+//  Created by Cori Mims on 5/26/17.
+//  Copyright © 2017 Cori Mims. All rights reserved.
+//
+
+import Foundation
---
+++ StarWarsAPI/Endpoints.swift
@@ -0,0 +1,46 @@
+//
+//  Endpoints.swift
+//  StarWarsAPI
+//
+//  Created by Cori Mims on 5/26/17.
+//  Copyright © 2017 Cori Mims. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
