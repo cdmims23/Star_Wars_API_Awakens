6096620a778a996d4cf260d4ae3f93866c28f616
Cori Mims <cdmims21@gmail.com>
2017-05-27 14:12:26 -0400

    Created the character model.

--- StarWarsAPI/Characters.swift
+++ StarWarsAPI/Characters.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+class Characters {
+    let name: String
+    let birth_year: String
+    let height: String
+    let eye_color: String
+    let hair_color: String
+    
+    init(name: String, birth_year: String, height: String, eye_color: String, hair_color: String) {
+        self.name = name
+        self.birth_year = birth_year
+        self.height = height
+        self.eye_color = eye_color
+        self.hair_color = hair_color
+    }
+}
