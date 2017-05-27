d364f4b6a6be40328a77b014c593ac4d9bac53cc
Cori Mims <cdmims21@gmail.com>
2017-05-27 14:23:08 -0400

    Finished Vehicle and Starships model classes.

--- StarWarsAPI/Starships.swift
+++ StarWarsAPI/Starships.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+class Starships {
+    let name: String
+    let manufacturer: String
+    let cost: String
+    let length: String
+    let crew: String
+    let starship_class: String
+    
+    init(name: String, manufacturer: String, cost: String, length: String, crew: String, starship_class: String) {
+        self.name = name
+        self.manufacturer = manufacturer
+        self.cost = cost
+        self.length = length
+        self.crew = crew
+        self.starship_class = starship_class
+    }
+}
--- StarWarsAPI/Vehicles.swift
+++ StarWarsAPI/Vehicles.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+class Vehicle {
+    let name: String
+    let manufacturer: String
+    let cost: String
+    let length: String
+    let crew: String
+    let vehicle_class: String
+    
+    init(name: String, manufacturer: String, cost: String, length: String, crew: String, vehicle_class: String) {
+        self.name = name
+        self.manufacturer = manufacturer
+        self.cost = cost
+        self.length = length
+        self.crew = crew
+        self.vehicle_class = vehicle_class
+    }
+}
